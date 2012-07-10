7fb15e4d32a7b62142c206bf4242745373bba7e7
森井ゴンザレス <morygonzalez@gmail.com>
2012-07-10 19:39:26 +0900

    Removes white space

--- content.coffee
+++ content.coffee
@@ -1,6 +1,7 @@
 DiaryBody = $('.diary_body').text()
-  .replace(/^\s+/, '')
+  .replace(/^\s+?([^\s]+)/, "$1")
+  .replace(/\s+$/, '')
   .replace(/([^\n])\n([^\n])/g, "$1  \n$2")
 converter = new Showdown.converter
 DiaryBodyHtml = converter.makeHtml DiaryBody
-$('.diary_body').replaceWith('<div class="diary_body">' + DiaryBodyHtml + '</div>')
+$('.diary_body').replaceWith('<div class="diary_body parsed">' + DiaryBodyHtml + '</div>')
--- content.js
+++ content.js
@@ -2,12 +2,12 @@
 (function() {
   var DiaryBody, DiaryBodyHtml, converter;
 
-  DiaryBody = $('.diary_body').text().replace(/^\s+/, '').replace(/([^\n])\n([^\n])/g, "$1  \n$2");
+  DiaryBody = $('.diary_body').text().replace(/^\s+?([^\s]+)/, "$1").replace(/\s+$/, '').replace(/([^\n])\n([^\n])/g, "$1  \n$2");
 
   converter = new Showdown.converter;
 
   DiaryBodyHtml = converter.makeHtml(DiaryBody);
 
-  $('.diary_body').replaceWith('<div class="diary_body">' + DiaryBodyHtml + '</div>');
+  $('.diary_body').replaceWith('<div class="diary_body parsed">' + DiaryBodyHtml + '</div>');
 
 }).call(this);
